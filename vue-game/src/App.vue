1fe7b43a66f4c4f3a98b938f8db10a5dacbd3d8e
Vladimir Orlov <satancrew.true@gmail.com>
2023-08-02 12:47:34 +0300

    feat: add logic for entered letters

--- vue-game/src/App.vue
+++ vue-game/src/App.vue
@@ -5,15 +5,34 @@ import GameWrongLetter from './components/GameWrongLetter.vue'
 import GameWord from './components/GameWord.vue'
 import GamePopup from './components/GamePopup.vue'
 import GameNotification from './components/GameNotification.vue'
+import { computed, ref } from 'vue'
+
+const word = ref('василий')
+const letters = ref<string[]>([])
+const correctLetters = computed(() => letters.value.filter((el) => word.value.includes(el)))
+const wrongLetters = computed(() => letters.value.filter((el) => !word.value.includes(el)))
+const notification = ref<InstanceType<typeof GameNotification> | null>(null)
+
+window.addEventListener('keydown', ({ key }) => {
+  if (letters.value.includes(key)) {
+    notification.value?.openPopup()
+    setTimeout(() => notification.value?.closePopup(), 1500)
+    return
+  }
+
+  if (/[а-яА-ЯёЁ]/.test(key)) {
+    letters.value.push(key.toLowerCase())
+  }
+})
 </script>
 
 <template>
   <GameHeader />
   <div class="game-container">
     <GameFigure />
-    <GameWrongLetter />
-    <GameWord />
+    <GameWrongLetter :wrong-letters="wrongLetters" />
+    <GameWord :word="word" :correct-letters="correctLetters" />
   </div>
   <GamePopup v-if="false" />
-  <GameNotification />
+  <GameNotification ref="notification" />
 </template>
--- vue-game/src/components/GameNotification.vue
+++ vue-game/src/components/GameNotification.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isVisible = ref(false)
+
+const openPopup = () => {
+  isVisible.value = true
+}
+
+const closePopup = () => {
+  isVisible.value = false
+}
+
+defineExpose({
+  openPopup,
+  closePopup
+})
+</script>
+
 <template>
-  <div class="notification-container show">
+  <div class="notification-container" :class="{ show: isVisible }">
     <p>Вы уже вводили этот символ</p>
   </div>
 </template>
